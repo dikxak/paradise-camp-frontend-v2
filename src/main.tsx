01ce3cc46e70ea8802823001f2417d78bea2ea73
Dikshak Poudel <89897583+dikxak@users.noreply.github.com>
2024-07-19 22:19:56 +0545

    Setup/SCSS and stylelint (#2)
    * finish adding sass and stylelint configuration to the project
    
    * update lint-staged to include stylelint command

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 
 import App from "@/app/App";
 
+import "@/assets/scss/main.scss";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
