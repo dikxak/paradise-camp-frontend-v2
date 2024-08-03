933aa701547a5d1fed4ba82f37fcb715457512f4
Dikshak Poudel <89897583+dikxak@users.noreply.github.com>
2024-08-03 18:48:14 +0545

    finish setting up i18next in the project (#11)

---
+++ src/lib/i18n/en.ts
@@ -0,0 +1,16 @@
+// NOTE: Please maintain the alphabetical order while adding the items; it will be easier to search and look through the file in that way.
+
+const en = {
+  translation: {
+    welcome: {
+      message: "Welcome to ParadiseCamp",
+    },
+    common: {},
+    fetchError: {},
+    modal: {},
+    heading: {},
+    toasterMessage: {},
+  },
+};
+
+export default en;
